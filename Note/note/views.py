0117866f9daf432b9aab9b8d185f7904551f7727
seallor <sstrannik42@gmail.com>
2018-07-04 09:00:09 +0300

    Добавлены форма. Рабочая версия

--- Note/note/views.py
+++ Note/note/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from .models import Note
+from .forms import NoteForm
+from django.shortcuts import redirect
 
 
 def index(request):
-    notes = Note.objects.order_by('unique_words')
-    return render(request, 'note/index.html', {'notes': notes})
+    notes = Note.objects.order_by('-unique_words')
+    if request.method == "POST":
+
+        form = NoteForm(request.POST)
+        if form.is_valid():
+
+            post = form.save(commit=False)
+            post.publish()
+            return redirect('index')
+
+    else:
+        form = NoteForm()
+
+    return render(request, 'note/index.html', {'notes': notes, 'form': form})
+
+
 # Create your views here.
